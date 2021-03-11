7f04b3ee5fd25205ba95a1f3063673f94ef8bdd1
levinhne <levinhne@gmail.com>
2021-03-11 14:02:49 +0700

    update code

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,7 +70,7 @@ const GeneratorPage: React.FC = () => {
 
     const handleCopy = () => {
         new ClipboardJS(".icon-copy", {
-            text: (trigger) => {
+            text: () => {
                 setIconActions({
                     ...iconActions,
                     clipboard: {
