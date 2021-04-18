ca8d3c5e33178093df626d3d096c8e9c36a42701
levinhne <levinhne@gmail.com>
2021-04-18 09:04:14 +0700

    update code

--- api/client.ts
+++ api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import queryString from 'query-string';
 
 const client = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://strongpasswordsgenerator.net/api",
     headers: {
         "content-type": "application/json"
     },
