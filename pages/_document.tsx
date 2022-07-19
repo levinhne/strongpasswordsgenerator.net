7f91fea9a77db705d7a450eb0c251f16b82508fc
vinhlq <vinhlq@smartosc.com>
2022-07-19 11:13:24 +0700

    update code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,6 +26,8 @@ class MyDocument extends Document {
                                     `,
                                 }}
                             />
+                            <script async={true} src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9880367516969749"
+                                crossOrigin="anonymous"></script>
                         </>
                     )}
                 </Head>
